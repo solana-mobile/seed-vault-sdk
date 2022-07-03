f4311cc186ba1c8ec32fd595c02f181ebb901935
Continuous Integration <>
2022-07-03 04:24:50 +0000

    Update seedvault javadoc

--- seedvault/javadoc/package-search-index.js
+++ seedvault/javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.solanamobile.seedvault"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.solanamobile.seedvault"}]
