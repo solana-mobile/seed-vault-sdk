0657328944d68f396448c19259862b8084f0d866
Steven Laver <steven.laver@solana.com>
2022-08-30 18:32:42 -0700

    fakewallet can now test up to (or exceeding) implementation limits
    Fixes #78

--- fakewallet/src/main/java/com/solanamobile/fakewallet/ui/SeedListAdapter.kt
+++ fakewallet/src/main/java/com/solanamobile/fakewallet/ui/SeedListAdapter.kt
@@ -9,23 +9,31 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.solanamobile.fakewallet.R
 import com.solanamobile.fakewallet.databinding.ItemSeedBinding
 import com.solanamobile.fakewallet.usecase.SeedPurposeUseCase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 
 class SeedListAdapter(
+    private val lifecycleScope: CoroutineScope,
+    private val implementationLimits: Flow<ImplementationLimits>,
     private val onSignTransaction: (Seed, Account) -> Unit,
     private val onAccountNameUpdated: (Seed, Account, String) -> Unit,
     private val onDeauthorizeSeed: (Seed) -> Unit,
-    private val onRequestPublicKeyForM1000HAndM1001H: (Seed) -> Unit,
-    private val onSignTwoTransactionsWithTwoSignatures: (Seed) -> Unit
+    private val onRequestPublicKeys: (Seed) -> Unit,
+    private val onSignMaxTransactionsWithMaxSignatures: (Seed) -> Unit
 ) : ListAdapter<Seed, SeedListAdapter.SeedViewHolder>(SeedDiffCallback) {
-    class SeedViewHolder(
-        private val binding: ItemSeedBinding,
-        private val onSignTransaction: (Seed, Account) -> Unit,
-        private val onAccountNameUpdated: (Seed, Account, String) -> Unit,
-        private val onDeauthorizeSeed: (Seed) -> Unit,
-        private val onRequestPublicKeyForM1000HAndM1001H: (Seed) -> Unit,
-        private val onSignTwoTransactionsWithTwoSignatures: (Seed) -> Unit
+    data class ImplementationLimits(
+        val maxSigningRequests: Int,
+        val maxRequestedSignatures: Int,
+        val firstRequestedPublicKey: String,
+        val lastRequestedPublicKey: String
+    )
+
+    inner class SeedViewHolder(
+        private val binding: ItemSeedBinding
     ) : RecyclerView.ViewHolder(binding.root) {
         private var seed: Seed? = null
         private val adapter: AccountListAdapter = AccountListAdapter(
@@ -42,19 +50,35 @@ class SeedListAdapter(
         )
 
         init {
+            lifecycleScope.launch {
+                implementationLimits.collect {
+                    binding.buttonSignMaxTransactionsWithMaxSignatures.text =
+                        binding.root.context.getString(
+                            R.string.action_sign_max_transactions_with_max_signatures,
+                            it.maxSigningRequests,
+                            it.maxRequestedSignatures
+                        )
+                    binding.buttonRequestPublicKeys.text = binding.root.context.getString(
+                        R.string.action_request_public_keys,
+                        it.firstRequestedPublicKey,
+                        it.lastRequestedPublicKey
+                    )
+                }
+            }
+
             binding.buttonDeauthorizeSeed.setOnClickListener {
                 seed?.let { s ->
                     onDeauthorizeSeed(s)
                 }
             }
-            binding.buttonRequestTwoPublicKeys.setOnClickListener {
+            binding.buttonRequestPublicKeys.setOnClickListener {
                 seed?.let { s ->
-                    onRequestPublicKeyForM1000HAndM1001H(s)
+                    onRequestPublicKeys(s)
                 }
             }
-            binding.buttonSignTwoTransactionsWithTwoSignatures.setOnClickListener {
+            binding.buttonSignMaxTransactionsWithMaxSignatures.setOnClickListener {
                 seed?.let { s ->
-                    onSignTwoTransactionsWithTwoSignatures(s)
+                    onSignMaxTransactionsWithMaxSignatures(s)
                 }
             }
             binding.recyclerviewAccounts.adapter = adapter
@@ -74,8 +98,7 @@ class SeedListAdapter(
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SeedViewHolder {
         val binding = ItemSeedBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return SeedViewHolder(binding, onSignTransaction, onAccountNameUpdated, onDeauthorizeSeed,
-            onRequestPublicKeyForM1000HAndM1001H, onSignTwoTransactionsWithTwoSignatures)
+        return SeedViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: SeedViewHolder, position: Int) {
