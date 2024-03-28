26c0b1fe97b9e802f265e1737ea27db80d70d8dd
Funkatronics <funkatronicsmail@gmail.com>
2024-03-27 18:58:10 -0600

    some cleanup

--- fakewalletreact/App.tsx
+++ fakewalletreact/App.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
-import {
-  PermissionsAndroid,
-  View,
-} from 'react-native';
+import { View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { SeedVaultPermissionAndroid } from '@solana-mobile/seed-vault-lib';
 
 import MainScreen from './screens/MainScreen';
 
 export default function App() {
-  return (
-    <SafeAreaProvider>
-      <View>
-        <MainScreen />
-      </View>
-    </SafeAreaProvider>
-  );
+	return (
+		<SafeAreaProvider>
+		<View>
+			<MainScreen />
+		</View>
+		</SafeAreaProvider>
+	);
 }
