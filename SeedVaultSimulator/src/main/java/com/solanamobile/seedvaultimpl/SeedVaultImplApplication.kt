0171a0c0d733f898a185f92df154cb2f6286e260
sdlaver <103003665+sdlaver@users.noreply.github.com>
2024-11-27 11:09:28 -0800

    Fix a bug with showSeedSettings result handling (#375)

--- SeedVaultSimulator/src/main/java/com/solanamobile/seedvaultimpl/SeedVaultImplApplication.kt
+++ SeedVaultSimulator/src/main/java/com/solanamobile/seedvaultimpl/SeedVaultImplApplication.kt
@@ -5,14 +5,20 @@
 package com.solanamobile.seedvaultimpl
 
 import android.app.Application
+import com.solanamobile.seedvaultimpl.data.SeedRepository
 import dagger.hilt.android.HiltAndroidApp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import javax.inject.Inject
 
 @HiltAndroidApp
 class SeedVaultImplApplication : Application() {
+    @Inject
+    lateinit var seedRepository: SeedRepository
+
+    // TODO: remove dependencyContainer in favor of hilt/dagger injection
     val dependencyContainer: ApplicationDependencyContainer by lazy {
-        ApplicationDependencyContainer(this)
+        ApplicationDependencyContainer(seedRepository)
     }
 }
