026c092480d7363e4835df8ddf8f1d398781e39d
sdlaver <103003665+sdlaver@users.noreply.github.com>
2025-05-01 15:00:06 -0700

    Skip the ipstf and ipsmf tests on implementations where PIN failure wipes the seed vault (#501)

--- cts/src/main/java/com/solanamobile/seedvault/cts/data/testdata/ImplementationDetails.kt
+++ cts/src/main/java/com/solanamobile/seedvault/cts/data/testdata/ImplementationDetails.kt
@@ -15,50 +15,54 @@ import dagger.hilt.components.SingletonComponent
 internal sealed interface ImplementationDetails {
     fun generateSeedName(@IntRange(from = 0) i: Int): String
     val IS_PIN_CONFIGURABLE_PER_SEED: Boolean
+    val DOES_PIN_FAILURE_WIPE_SEED_VAULT: Boolean
     val MAX_SIGNING_REQUESTS: Int
     val MAX_REQUESTED_SIGNATURES: Int
     val MAX_REQUESTED_PUBLIC_KEYS: Int
     val IS_LEGACY_IMPLEMENTATION: Boolean
+}
 
-    data object Generic : ImplementationDetails {
-        override fun generateSeedName(i: Int) = "Test ${i + 1}"
-        override val IS_PIN_CONFIGURABLE_PER_SEED: Boolean = true
-        override val MAX_SIGNING_REQUESTS: Int = 3
-        override val MAX_REQUESTED_SIGNATURES: Int = 3
-        override val MAX_REQUESTED_PUBLIC_KEYS: Int = 10
-        override val IS_LEGACY_IMPLEMENTATION: Boolean = false
-    }
+private data object Generic : ImplementationDetails {
+    override fun generateSeedName(i: Int) = "Test ${i + 1}"
+    override val IS_PIN_CONFIGURABLE_PER_SEED: Boolean = true
+    override val DOES_PIN_FAILURE_WIPE_SEED_VAULT: Boolean = false
+    override val MAX_SIGNING_REQUESTS: Int = 3
+    override val MAX_REQUESTED_SIGNATURES: Int = 3
+    override val MAX_REQUESTED_PUBLIC_KEYS: Int = 10
+    override val IS_LEGACY_IMPLEMENTATION: Boolean = false
+}
 
-    data object Saga : ImplementationDetails {
-        override fun generateSeedName(i: Int): String = if (i == 0) {
-            "My Saga Seed"
-        } else {
-            "My Saga Seed ${i + 1}"
-        }
-        override val IS_PIN_CONFIGURABLE_PER_SEED: Boolean = true
-        override val MAX_SIGNING_REQUESTS: Int = 3
-        override val MAX_REQUESTED_SIGNATURES: Int = 3
-        override val MAX_REQUESTED_PUBLIC_KEYS: Int = 10
-        override val IS_LEGACY_IMPLEMENTATION: Boolean = true
+private data object Saga : ImplementationDetails {
+    override fun generateSeedName(i: Int): String = if (i == 0) {
+        "My Saga Seed"
+    } else {
+        "My Saga Seed ${i + 1}"
     }
+    override val IS_PIN_CONFIGURABLE_PER_SEED: Boolean = true
+    override val DOES_PIN_FAILURE_WIPE_SEED_VAULT: Boolean = true
+    override val MAX_SIGNING_REQUESTS: Int = 3
+    override val MAX_REQUESTED_SIGNATURES: Int = 3
+    override val MAX_REQUESTED_PUBLIC_KEYS: Int = 10
+    override val IS_LEGACY_IMPLEMENTATION: Boolean = true
+}
 
-    sealed class SeekerCommon : ImplementationDetails {
-        override fun generateSeedName(i: Int): String = "Seeker Seed ${i + 1}"
-        override val IS_PIN_CONFIGURABLE_PER_SEED: Boolean = false
-        override val IS_LEGACY_IMPLEMENTATION: Boolean = false
-    }
+private sealed class SeekerCommon : ImplementationDetails {
+    override fun generateSeedName(i: Int): String = "Seeker Seed ${i + 1}"
+    override val IS_PIN_CONFIGURABLE_PER_SEED: Boolean = false
+    override val DOES_PIN_FAILURE_WIPE_SEED_VAULT: Boolean = true
+    override val IS_LEGACY_IMPLEMENTATION: Boolean = false
+}
 
-    data object SeekerStandard : SeekerCommon() {
-        override val MAX_SIGNING_REQUESTS: Int = 3
-        override val MAX_REQUESTED_SIGNATURES: Int = 3
-        override val MAX_REQUESTED_PUBLIC_KEYS: Int = 10
-    }
+private data object SeekerStandard : SeekerCommon() {
+    override val MAX_SIGNING_REQUESTS: Int = 3
+    override val MAX_REQUESTED_SIGNATURES: Int = 3
+    override val MAX_REQUESTED_PUBLIC_KEYS: Int = 10
+}
 
-    data object SeekerPrivileged : SeekerCommon() {
-        override val MAX_SIGNING_REQUESTS: Int = 10
-        override val MAX_REQUESTED_SIGNATURES: Int = 3
-        override val MAX_REQUESTED_PUBLIC_KEYS: Int = 30
-    }
+private data object SeekerPrivileged : SeekerCommon() {
+    override val MAX_SIGNING_REQUESTS: Int = 10
+    override val MAX_REQUESTED_SIGNATURES: Int = 3
+    override val MAX_REQUESTED_PUBLIC_KEYS: Int = 30
 }
 
 @Module
@@ -67,12 +71,12 @@ internal object ImplementationDetailsModule {
     @Provides
     fun provideImplementationDetails() : ImplementationDetails {
         return when (Build.MODEL) {
-            "Saga" -> ImplementationDetails.Saga
+            "Saga" -> Saga
             "Seeker" -> when (BuildConfig.FLAVOR) {
-                "Privileged" -> ImplementationDetails.SeekerPrivileged
-                else -> ImplementationDetails.SeekerStandard
+                "Privileged" -> SeekerPrivileged
+                else -> SeekerStandard
             }
-            else -> ImplementationDetails.Generic
+            else -> Generic
         }
     }
 }
